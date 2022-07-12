a6925e2812d2420470aa3a7f38d2401a2ee8e82a
Guariko <creatingsomething2021@gmail.com>
2022-07-12 20:21:49 -0300

    Portfolio complete

--- index.js
+++ index.js
@@ -1,51 +1,59 @@
 // TODO: Functions start */
 
 function show(element, classToShowElement) {
-    element.classList.toggle(classToShowElement);
+  element.classList.toggle(classToShowElement);
 }
 
 function hide(element, classToBeRemoved) {
-    element.classList.remove(classToBeRemoved);
+  element.classList.remove(classToBeRemoved);
 }
 
 // FIXME: Functions end //
 
 // TODO: Header starts //
 
-const headerNavBarContainer = document.querySelector(".header__nav__bar__container");
-const closeHeaderNavBar = document.querySelector(".header__nav__bar__container .fa-times");
+const headerNavBarContainer = document.querySelector(
+  ".header__nav__bar__container"
+);
+const closeHeaderNavBar = document.querySelector(
+  ".header__nav__bar__container .fa-times"
+);
 const showHeaderNavBar = document.querySelector(".header .fa-bars");
 const classToShowElement = "active";
-const headerNavBarItems = document.querySelectorAll(".header__nav__bar__item a");
+const headerNavBarItems = document.querySelectorAll(
+  ".header__nav__bar__item a"
+);
 
-
-showHeaderNavBar.addEventListener("click", e => {
-    show(headerNavBarContainer, classToShowElement);
+showHeaderNavBar.addEventListener("click", (e) => {
+  show(headerNavBarContainer, classToShowElement);
 });
 
-closeHeaderNavBar.addEventListener("click", e => {
+closeHeaderNavBar.addEventListener("click", (e) => {
+  hide(headerNavBarContainer, classToShowElement);
+});
+
+headerNavBarItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
     hide(headerNavBarContainer, classToShowElement);
-});
-
-headerNavBarItems.forEach(item => {
-    item.addEventListener("click", e => {
-        hide(headerNavBarContainer, classToShowElement);
-    });
+  });
 });
 
 // FIXME: Header ends //
 
 // TODO: Swiper starts //
 
-var swiper = new Swiper(".project__images", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop: true,
-  });
+var swiper = new Swiper(".project__swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-// FIXME: Swiper ends //  
+var swiper = new Swiper(".mobile__project__swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// FIXME: Swiper ends //
